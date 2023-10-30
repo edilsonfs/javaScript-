8a56d0af7ecd7d0e3c2593d65638deb56c54f159
devferreira <edilson.mx@gmail.com>
2023-10-30 00:05:24 -0300

    novo commit

---
+++ Aula1/app.js
@@ -0,0 +1,13 @@
+/**
+ * Aula 06
+ * No JavaScript a decolação de variáveis se dá pelo comando 'let'
+ * quando é necessário que o valor declarado no programa seja constante durante 
+ * a execução é criaado uma constante com a palavra reservada 'const', nesse
+ * caso se houver uma nova atribuição de valor a constante, haverá um erro na
+ * execução do programa. 
+ */
+// let age = 35
+// let currentYear = 2023
+// const anoNascimento = 1988
+// console.log(age, currentYear)
+
